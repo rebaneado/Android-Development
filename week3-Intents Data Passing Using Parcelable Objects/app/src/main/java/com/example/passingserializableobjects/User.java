795154d6e4b18436cfc24df15565bae4eabd616b
Hugo <hamolinagomez@gmail.com>
2023-02-07 17:46:39 -0500

    implemented parcel methods

--- week3-Intents Data Passing Using Parcelable Objects/app/src/main/java/com/example/passingserializableobjects/User.java
+++ week3-Intents Data Passing Using Parcelable Objects/app/src/main/java/com/example/passingserializableobjects/User.java
@@ -1,7 +1,12 @@
 package com.example.passingserializableobjects;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
 
-public class User implements Serializable{
+public class User implements Parcelable {
     String name;
     int age;
 
@@ -14,4 +19,32 @@ public class User implements Serializable{
     }
 
 
+    protected User(Parcel in) {
+        name = in.readString();
+        age = in.readInt();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeInt(this.age);
+
+    }
 }
